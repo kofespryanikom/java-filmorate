755709f0f9147b8444c02f4aa1a52888d3d355c4
Artem Ledovskikh <ledartem@bk.ru>
2026-01-25 13:25:00 +0700

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/user/UserDbStorage.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.InternalServerErrorException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.user.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.storage.dao.BaseRepository;
