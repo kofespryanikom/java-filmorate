237bb83eaf9c76c279ac2d9362267af1fda92b46
Danny <danny.koshik@gmail.com>
2026-01-29 18:25:56 +0300

    fix: fix Checkstyle in DirectorService

--- src/main/java/ru/yandex/practicum/filmorate/service/film/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/DirectorService.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.film.Director;
 import java.util.List;
 
 public interface DirectorService {
+
     List<Director> findAll();
+
     Director findById(Integer id);
+
     Director createDirector(Director director);
+
     Director updateDirector(Director director);
+
     void deleteDirector(Integer id);
 }
