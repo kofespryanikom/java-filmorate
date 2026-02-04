236d8e6096a55c4980c60c495b3d9b6b2aabcf67
Danny <danny.koshik@gmail.com>
2026-02-04 23:49:50 +0300

    fix: финальное исправление для сортировки по лайку и проверки режиссеров

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
@@ -92,11 +92,11 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
     private static final String FIND_BY_DIRECTOR_SORT_LIKES =
             "SELECT f.film_id " +
             "FROM films f " +
-            "LEFT JOIN users_liked ul ON f.film_id = ul.film_id " +
             "JOIN film_directors fd ON f.film_id = fd.film_id " +
+            "LEFT JOIN users_liked ul ON f.film_id = ul.film_id " +
             "WHERE fd.director_id = ? " +
             "GROUP BY f.film_id " +
-            "ORDER BY COUNT(ul.user_id) DESC";
+            "ORDER BY COUNT(ul.user_id) DESC, f.release_date ASC";
 
     private final UserStorage userStorage;
     private final DirectorDbStorage directorStorage;
