c1d9778c6038c582af64946524f19c11d7e166a0
Daniel <danny.koshik@gmail.com>
2026-02-07 14:34:43 +0300

    Update FilmDbStorage.java
    Fix: delete not in used code

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
@@ -88,15 +88,7 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
             "JOIN film_directors fd ON f.film_id = fd.film_id " +
             "WHERE fd.director_id = ? " +
             "ORDER BY f.release_date ASC";
-
-    //private static final String FIND_BY_DIRECTOR_SORT_LIKES =
-    //            "SELECT f.film_id FROM films f " +
-    //            "JOIN film_directors fd ON f.film_id = fd.film_id " +
-    //            "LEFT JOIN users_liked ul ON f.film_id = ul.film_id " +
-    //            "WHERE fd.director_id = ? " +
-    //            "GROUP BY f.film_id " +
-    //            "ORDER BY COUNT(ul.user_id) DESC, f.release_date DESC, f.film_id ASC";
-
+    
     private static final String FIND_BY_DIRECTOR_SORT_LIKES =
             "SELECT f.film_id " +
             "FROM films AS f " +
@@ -105,13 +97,7 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
             "WHERE fd.director_id = ? " +
             "GROUP BY f.film_id " +
             "ORDER BY COUNT(ul.user_id) DESC, f.film_id ASC";
-           // "SELECT f.film_id FROM films f " +
-          //  "LEFT JOIN users_liked ul ON f.film_id = ul.film_id " +
-          //  "JOIN film_directors fd ON f.film_id = fd.film_id " +
-         //   "WHERE fd.director_id = ? " +
-         //   "GROUP BY f.film_id " +
-         //   "ORDER BY COUNT(ul.user_id) DESC, f.film_id ASC";
-
+           
     private final UserStorage userStorage;
     private final DirectorDbStorage directorStorage;
 
