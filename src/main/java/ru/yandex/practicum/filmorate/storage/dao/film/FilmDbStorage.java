dd338f96c850237ab4aa00c14bfd10bb02e300a6
Daniel <danny.koshik@gmail.com>
2026-02-07 11:39:47 +0300

    Update FilmDbStorage.java
    Fix: SQL Sort_Likes

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
@@ -98,12 +98,14 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
     //            "ORDER BY COUNT(ul.user_id) DESC, f.release_date DESC, f.film_id ASC";
 
     private static final String FIND_BY_DIRECTOR_SORT_LIKES =
-        "SELECT f.film_id FROM films f " +
-        "LEFT JOIN users_liked ul ON f.film_id = ul.film_id " +
-        "JOIN film_directors fd ON f.film_id = fd.film_id " +
+        "SELECT f.*, m.name AS mpa_name, COUNT(fl.user_id) AS likes " +
+        "FROM films AS f " +
+        "JOIN film_directors AS fd ON f.film_id = fd.film_id " +
+        "LEFT JOIN mpa_ratings AS m ON f.mpa_id = m.mpa_id " +
+        "LEFT JOIN film_likes AS fl ON f.film_id = fl.film_id " +
         "WHERE fd.director_id = ? " +
         "GROUP BY f.film_id " +
-        "ORDER BY COUNT(ul.user_id) DESC";
+        "ORDER BY likes DESC";
 
     private final UserStorage userStorage;
     private final DirectorDbStorage directorStorage;
