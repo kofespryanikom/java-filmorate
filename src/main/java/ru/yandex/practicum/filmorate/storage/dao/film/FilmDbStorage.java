9caffaf052423d09d48c8167d08ba4b55b7daa2b
Daniel <danny.koshik@gmail.com>
2026-02-07 17:50:18 +0300

    Update FilmDbStorage.java
    Финал

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
@@ -137,7 +137,7 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
                 f.setGenres(new LinkedHashSet<>());
             }
             if (f.getDirectors() == null) {
-                f.setDirectors(new ArrayList<>());
+                f.setDirectors(new HashSet<>());
             }
         }
 
