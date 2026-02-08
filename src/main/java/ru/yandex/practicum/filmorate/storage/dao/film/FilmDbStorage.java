12fddd50cd052c990c55124f1cd6f3e119a500ce
Daniel <danny.koshik@gmail.com>
2026-02-08 16:04:15 +0300

    Update FilmDbStorage.java
    Финал?

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/film/FilmDbStorage.java
@@ -92,7 +92,7 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
             "JOIN film_directors fd ON f.film_id = fd.film_id " +
             "WHERE fd.director_id = ? " +
             "GROUP BY f.film_id " +
-            "ORDER BY COUNT(ul.user_id) DESC, f.release_date DESC, f.film_id ASC";
+            "ORDER BY COUNT(ul.user_id) DESC, f.film_id ASC";// f.release_date DESC, 
            // "SELECT f.film_id " +
           //  "FROM films AS f " +
          //   "LEFT JOIN users_liked AS ul ON f.film_id = ul.film_id " +
