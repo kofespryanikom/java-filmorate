45b7dba3218423baac071471c6eee3631d57ec1a
Danny <danny.koshik@gmail.com>
2026-02-10 19:04:20 +0300

    fix: финальное исправление

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
@@ -1,4 +1,5 @@
 package ru.yandex.practicum.filmorate.service.film;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
